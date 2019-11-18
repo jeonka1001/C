3a7c15af9d84fdefa9e90587381838b78c9dc12b
jeonka1001 <52381846+jeonka1001@users.noreply.github.com>
2019-11-18 16:19:45 +0900

    Add files via upload

---
+++ HW11.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  HW11_20190702
+//
+//  Created by 전경안 on 02/07/2019.
+//  Copyright © 2019 전경안. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("ASCII code 값을 입력하시오:");
+    scanf("%d",&a);
+    printf("%d 은 %c 의 ASCII code 입니다.\n",a,a);
+    return 0;
+}
