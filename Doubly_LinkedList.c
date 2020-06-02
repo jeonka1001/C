367035d62d132d335fecf19ac3e22b0b3bf3fff0
jeonka1001 <52381846+jeonka1001@users.noreply.github.com>
2020-06-02 21:42:17 +0900

    Update Doubly_LinkedList.c

--- Doubly_LinkedList.c
+++ Doubly_LinkedList.c
@@ -7,160 +7,123 @@
 //
 
 //이중 연결 리스트
+//
+//  A.c
+//  Day_0602
+//
+//  Created by 전경안 on 2020/06/02.
+//  Copyright © 2020 전경안. All rights reserved.
+//
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct Node{
-    int data;
+#define list_entry(ptr,type,member) (type*)((char*)ptr - (long)&((type*)0)->member);
+
+#define list_foreach(node,head) for (node = (head)->prev; node != (head); node = node->prev)
+
+#define list_foreach_reverse(node,head) for (node = (head)->prev; node != (head); node = node->prev)
+
+#define LIST_HEAD(name) Node name = { &(name),&(name) }
+
+typedef struct Node {
     struct Node* next;
     struct Node* prev;
-}Node;
+} Node;
 
-Node *head;
-Node *tail;
-int count;
-
-int listInitialize(){
-    head = calloc(1,sizeof(Node));
-    if(head== NULL){
-        perror(__func__);
-        return -1;
-    }
-    tail = calloc(1,sizeof(Node));
-    if(tail == NULL){
-        perror(__func__);
-        return -1;
-    }
-    head->prev = head;
-    head->next = tail;
-    
-    tail->prev = head;
-    tail->next = tail;
-    count =0;
-    return 0;
+static inline void insertNode(Node* new, Node* prev, Node* next) {
+    new->next = next;
+    new->prev = prev;
+    prev->next = new;
+    next->prev = new;
 }
 
-int addFront(int data){
-    Node *node = malloc(sizeof(Node));
-    node->data = data;
-    node->prev = head;
-    node->next = head->next;
-    
-    head->next->prev = node;
-    head->next = node;
-    
-    count ++;
-    return 0;
+static inline void listAdd(Node* head, Node* node) {
+    insertNode(node, head->prev, head);
+    return ;
 }
 
-int addBack(int data){
-    Node *node = malloc(sizeof(Node));
-    node->data = data;
-    node->next = tail;
-    node->prev=tail->prev;
-    
-    tail->prev->next = node;
-    tail->prev = node;
-    
-    count ++;
-    return 0;
+static inline void listAddHead(Node* head, Node* node) {
+    insertNode(node, head, head->next);
+    return ;
+}
+static inline void listRemove(Node *node){
+    Node *prev = node->prev;
+    Node *next = node->next;
+    next->prev = prev;
+    prev->next = next;
+    return ;
 }
 
-int dataInsert(int data, int index){
-    Node *node = malloc(sizeof(Node));
-    node->data=data;
-    
-    Node *cur ;
-    if(count/2 >index){
-        cur = head;
-        for(int i = 1; i<=index;i++){
-            cur = cur->next;
-        }
-    }
-    else{
-        cur = tail;
-        for(int i = count; i >= index ; i--){
-            cur=cur->prev;
-        }
-    }
-    node->prev = cur->prev;
-    node->next = cur;
-    cur->prev->next = node;
-    cur->prev = node;
-    count ++;
-    return 0;
-}
 
-int dataUpdate(int newData, int *oldData, int index){
-    Node *cur ;
-    if(count/2 >index){
-        cur = head;
-        for(int i = 1; i<=index;i++){
-            cur = cur->next;
-        }
+// 위의 코드는 라이브러리가 제공하는 코드입니다.
+// ----------------------------------------------------------------------------------
+// 아래의 코드는 사용자가 구현하는 코드입니다.
+
+
+typedef struct Person {
+    char name[32];
+    int age;
+    Node list;
+} Person;
+
+void display(const Node* head) {
+    if (head == NULL) {
+        fprintf(stderr, "display(): argument is null\n");
+        return;
     }
-    else{
-        cur = tail;
-        for(int i = count; i >= index ; i--){
-            cur=cur->prev;
-        }
-    }
-    *oldData = cur -> data;
-    cur->data=newData;
-    return 0;
-}
-int printList(){
+    
     system("clear");
-    Node *temp = head;
     printf("[head]");
-    while(temp->next != tail){
-        temp=temp->next;
-        printf("->[%2d]",temp->data);
+    Node *node;
+    list_foreach(node,head) {
+        Person* p = list_entry(node,Person,list);
+        printf("<->[%s(%d)]", p->name, p->age);
     }
-    printf("->[tail]");
+    printf("<->[head]");
     getchar();
-    return 0;
 }
 
-int dataDelete(int *oldData,int index){
-    Node *cur ;
-    if(count/2 >index){
-        cur = head;
-        for(int i = 1; i<=index;i++){
-            cur = cur->next;
-        }
+//이 전 코드는 자료구조 탐색이 너무 어렵다.
+// 이를 해결하기위해 탐색 함수를 제공합니다.
+void displayBackwardly(const Node* head) {
+    if (head == NULL) {
+        fprintf(stderr, "displayBackwardly(): argument is null\n");
+        return;
     }
-    else{
-        cur = tail;
-        for(int i = count; i >= index ; i--){
-            cur=cur->prev;
-        }
-    }
-    *oldData=cur->data;
-    cur->prev->next = cur->next;
-    cur->next->prev = cur->prev;
-    free(cur);
-    return 0;
-}
-
-int main(){
-    listInitialize();
-    printList();
-    for(int i = 0 ; i < 5; i ++){
-        addBack(i+1);
-        printList();
-    }
-    addFront(99);
-    printList();
-    dataInsert(77,3);
-    printList();
-    int oldData;
-    dataUpdate(25, &oldData, 5);
-    printList();
-    printf("이전 : %d, 변경 후 : 25\n",oldData);
-    dataDelete(&oldData, 2);
-    printf("삭제 : %d\n",oldData);
-    printList();
-    return 0;
     
+    system("clear");
+    printf("[head]");
+    Node* node;
+    list_foreach_reverse(node,head) {
+        Person* p = list_entry(node, Person, list);
+        printf("<->[%s(%d)]", p->name, p->age);
+    }
+    printf("<->[head]");
+    getchar();
 }
+
+int main(void) {
+    Person people[4] = {
+        {"eddy", 20}, {"pororo", 30}, {"petty", 40}, {"poby", 50}
+    };
+    
+    LIST_HEAD(head);
+    display(&head);
+    for (int i = 0; i < 4; i++) {
+        listAdd(&head, &(people[i].list));
+        // listAddHead(&head, nodes + i);
+        display(&head);
+    }
+    Node *node;
+    list_foreach(node,&head){
+        Person* p = list_entry(node,Person,list);
+        if(strcmp(p->name,"pororo")==0){
+            listRemove(node);
+        }
+    }
+    display(&head);
+    return 0;
+}
+
+
